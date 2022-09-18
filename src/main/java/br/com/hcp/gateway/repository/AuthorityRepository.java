5095fb759c59f9d0a661c7345fc5fae0b61bfc6a
Luciano Pacheco <luciano.pacheco@owse.com.br>
2022-09-18 12:08:06 -0300

    Initial commit

---
+++ src/main/java/br/com/hcp/gateway/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.hcp.gateway.repository;
+
+import br.com.hcp.gateway.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
---
+++ src/main/java/br/com/hcp/gateway/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package br.com.hcp.gateway.service.dto;
